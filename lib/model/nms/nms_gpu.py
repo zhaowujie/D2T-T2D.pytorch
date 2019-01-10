8cb9d9a84994d6aa5e18dc0a94bdbb44760d52ee
赵兴杰 <34273042+zhaowujie@users.noreply.github.com>
2019-01-10 15:01:02 +0800

    Add files via upload

---
+++ lib/model/nms/nms_gpu.py
@@ -0,0 +1,15 @@
+import sys
+import torch
+import numpy as np
+
+path = '/media/hp208/4t/zhaoxingjie/graduation_project/pytorch-detect-to-track/lib/model/nms'
+sys.path.insert(0, path)
+from _ext import nms
+import pdb
+
+def nms_gpu(dets, thresh):
+	keep = dets.new(dets.size(0), 1).zero_().int()
+	num_out = dets.new(1).zero_().int()
+	nms.nms_cuda(keep, dets, num_out, thresh)
+	keep = keep[:num_out[0]]
+	return keep
